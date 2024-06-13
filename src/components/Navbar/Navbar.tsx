e6cc9e8225ca0ab0fc5570bcf651248f32b2b191
MuratYvs <muratyvs99@gmail.com>
2024-06-13 11:03:31 +0300

    Update Navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import "../../Variables.css";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="bg-lime-50 border-gray-200 dark:bg-gray-900">
+      {/* <nav className="bg-lime-50 border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <p className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
@@ -35,11 +37,11 @@ const Navbar = () => {
             </svg>
           </button>
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <NavLink
                   to="/"
-                  className="block py-2 px-3 text-white hover:text-gray-400 rounded md:bg-transparent md:text-black md:p-0 dark:text-white md:dark:text-black"
+                  className="block py-2 px-3 hover:text-gray-400 rounded md:bg-transparent md:text-black md:p-0 dark:text-white md:dark:text-black"
                   aria-current="page"
                 >
                   Home
@@ -64,7 +66,24 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav> */}
+
+      <div>
+        <nav className={`${styles.header} bg-lime-50 main-padding`}>
+          <div className={`${styles.logo}`}>
+            <span>TrApple</span>
+          </div>
+
+          <div className={`${styles["header-links"]}`}>
+            <NavLink className={`${styles["link-element"]}`} to="/">
+              Hotels
+            </NavLink>
+            <NavLink className={`${styles["link-element"]}`} to="/">
+              Login
+            </NavLink>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
